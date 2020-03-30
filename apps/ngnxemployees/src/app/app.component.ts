1112af769b925a2108fc042dcbbffcefe93d66b0
Ruslan Lavreniuk <rla@ciklum.com>
2020-03-30 13:07:37 +0300

    ngnxemployees: + (blank project) init

---
+++ apps/ngnxemployees/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nxngemployees-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ngnxemployees';
+}
