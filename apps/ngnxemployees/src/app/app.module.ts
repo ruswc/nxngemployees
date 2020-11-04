a0be72423b7b18adac4d6cb088702a8308d595fc
Ruslan Lavreniuk <rla@ciklum.com>
2020-11-04 15:41:14 +0200

    feature/issue-2: + add ngrx store and ngrx devtools

--- apps/ngnxemployees/src/app/app.module.ts
+++ apps/ngnxemployees/src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 
@@ -22,7 +25,12 @@ const routes: Routes = [
         BrowserModule,
         BrowserAnimationsModule,
         RouterModule.forRoot(routes),
-        HttpClientModule
+        HttpClientModule,
+        StoreModule.forRoot({}, {}),
+        StoreDevtoolsModule.instrument({
+            maxAge: 30,
+            logOnly: environment.production
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
